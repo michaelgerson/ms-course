7603a85a1ec323320acbbfd3e32909200d2544fd
Michael <michael.aguiar@cs.inf.br>
2021-05-24 22:00:00 -0400

    Configuring Zuul api gateway

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -3,6 +3,8 @@ package com.devsuperior.hrapigatewayzuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
